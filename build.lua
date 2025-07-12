04758971ad7d1198753bea538cf5adfd6625ec99
Ed Sweeney <ed@onextent.com>
2025-07-11 22:09:58 -0700

    new build.lua

--- build.lua
+++ build.lua
@@ -7,15 +7,15 @@ return function()
 		return
 	end
 
-	local plugin_dir = vim.fn.stdpath("config") .. "/lazy/zim-sequencer"
-	local target = plugin_dir .. "/engine/target/release/zim-sequencer"
+	local plugin_root = debug.getinfo(1, "S").source:sub(2):match("(.*/)")
+	local target = plugin_root .. "engine/target/release/zim-sequencer"
 	local output = vim.fn.stdpath("data") .. "/zim-sequencer-bin"
 	vim.fn.mkdir(output, "p")
 
 	Job:new({
 		command = "cargo",
 		args = { "build", "--release" },
-		cwd = plugin_dir .. "/engine",
+		cwd = plugin_root .. "engine",
 		on_exit = function()
 			local dst = output .. "/zim-sequencer"
 			vim.fn.system({ "cp", target, dst })
