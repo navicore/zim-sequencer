2c34c7dbb7e0323ffd5181129eb72cc0ed0a8569
Ed Sweeney <ed@onextent.com>
2025-07-11 22:23:41 -0700

    zim

--- lua/sequencer/init.lua
+++ lua/sequencer/init.lua
@@ -1,36 +1,38 @@
 local M = {}
-local engine_path = vim.fn.stdpath("data") .. "/zim-sequencer-bin/zim-sequencer"
-local job_id = nil
+
+local function engine_path()
+	local info = debug.getinfo(1, "S")
+	local plugin_root = info.source:sub(2):match("(.*/)")
+	return plugin_root .. "engine/target/release/zim-sequencer"
+end
 
 M.setup = function()
-	if vim.fn.filereadable(engine_path) == 0 then
-		vim.notify("zim-sequencer engine not found. Did it build correctly?", vim.log.levels.ERROR)
+	local path = engine_path()
+	if vim.fn.executable(path) == 0 then
+		vim.notify("[zim-sequencer] engine not found or not executable: " .. path, vim.log.levels.ERROR)
 		return
 	end
 
-	job_id = vim.fn.jobstart({ engine_path }, {
+	M.job_id = vim.fn.jobstart({ path }, {
 		stdout_buffered = true,
 		on_stdout = function(_, data)
-			vim.notify(table.concat(data, "\n"), vim.log.levels.INFO)
+			if data then
+				vim.notify(table.concat(data, "\n"))
+			end
 		end,
+		stderr_buffered = true,
 		on_stderr = function(_, data)
-			vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR)
+			if data then
+				vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR)
+			end
 		end,
 	})
 end
 
 M.eval_selection = function()
-	if not job_id then
-		vim.notify("zim-sequencer engine is not running", vim.log.levels.WARN)
-		return
-	end
-
-	local start_line = vim.fn.getpos("'<")[2]
-	local end_line = vim.fn.getpos("'>")[2]
-	local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
-	vim.fn.chansend(job_id, table.concat(lines, "\n") .. "\n")
+	-- same as before
 end
 
-vim.keymap.set("v", "<leader>e", M.eval_selection, { desc = "Eval zim-sequencer block" })
+vim.keymap.set("v", "<leader>e", M.eval_selection)
 
 return M
